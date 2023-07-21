f7747237962269c792914261f1defa66b75d96d4
Luka <lukianeshavladze@gmail.com>
2023-07-21 15:15:46 +0400

    Added entity framework and created database

---
+++ HotelListing.API/Data/Hotel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Principal;
+
+namespace HotelListing.API.Data
+{
+    public class Hotel
+    {
+        
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Address { get; set; }
+
+        public double Rating { get; set; }
+
+        [ForeignKey(nameof(CountryID))]
+        public int CountryID { get; set; }
+        
+        public Country Country { get; set; }
+    }
+}
---
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Data
+{
+    public class HotelListingDbContext : DbContext
+    {
+        public HotelListingDbContext(DbContextOptions options) : base(options)
+        {
+                
+        }
+
+        public DbSet<Hotel> Hotels { get; set; }
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "Jamaica",
+                    ShortName = "JM"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "Bahamas",
+                    ShortName = "BS"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "Cayman Island",
+                    ShortName = "CI"
+                }
+
+            );
+
+            modelBuilder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "Sandals Resort and Spa",
+                    Address = "Negril",
+                    CountryID = 1,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "Comfort Suites",
+                    Address = "Georgi Town",
+                    CountryID = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "Grand Palldium",
+                    Address = "Nassua",
+                    CountryID = 2,
+                    Rating = 4
+                }
+
+                );
+
+        }
+    }
+}
